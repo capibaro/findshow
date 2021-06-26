72b4cd5688cbc54cfb1a33853130134f7e36078a
maotouyingxia <xiangxinxx@outlook.com>
2021-06-26 14:03:57 +0800

    free image host

--- findshow/settings.py
+++ findshow/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['web']
 # Application definition
 
 INSTALLED_APPS = [
+    'search.apps.SearchConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'haystack',
-    'search',
 ]
 
 MIDDLEWARE = [
